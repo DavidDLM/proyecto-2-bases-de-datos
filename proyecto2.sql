4325e89e0558fef195b8285f04f628a1b9234338
robertriosm <87707371+robertriosm@users.noreply.github.com>
2022-03-16 11:46:51 -0600

    adelantar tablas

--- proyecto2.sql
+++ proyecto2.sql
@@ -5,7 +5,7 @@ CREATE TABLE usuario (
 	first_name varchar(16) NOT NULL,
 	last_name varchar(16) NOT NULL,
 	region int NOT NULL, -- seran los country codes
-	type int NOT NULL, -- 0:gratuito, 1:pagado, 2:avanzado, si es gratuito, solo 1 perfil, pagado 4 perfiles y avanzado 8
+	plan_type int NOT NULL, -- 0:gratuito, 1:pagado, 2:avanzado, si es gratuito, solo 1 perfil, pagado 4 perfiles y avanzado 8
 	birthdate date NOT NULL,
 	profile1 varchar(12) NOT NULL,
 	profile2 varchar(12),
@@ -21,8 +21,16 @@ CREATE TABLE usuario (
 )
 
 CREATE TABLE serie (
-	serie_id varchar(12) NOT NULL,
+	serie_id varchar(12) PRIMARY KEY NOT NULL,
 	director varchar(20) NOT NULL,
-	category int NOT NULL, -- 0: todo publico, 1: mayores de 13, 2: mayores de 17
+	category int NOT NULL, -- 0: todo publico, 1: mayores de 10, 2: mayores de 13, 3: mayores de 15, 4: mayores de 17, 5: solo adultos
+	actor1 varchar(20) NOT NULL,
+	actor2 varchar(20) NOT NULL,
+	actor3 varchar(20) NOT NULL,
+	name varchar(30) NOT NULL,
+	award1 varchar(20) NOT NULL,
+	award2 varchar(20) NOT NULL,
+	award3 varchar(20) NOT NULL,
 )
 
+CREATE TABLE 
